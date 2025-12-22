0b8431ce8715622e8ec3f194fd2c6f7e0a08b6f1
stevenijm777 <yosoystevenijm@gmail.com>
2025-12-21 19:24:30 -0500

    finish ekf algorithm

--- packages/solution/ekf.py
+++ packages/solution/ekf.py
@@ -22,20 +22,23 @@ class EKF:
 
             # Step 1: update the pose estimate using the kinematic model
             # TODO: Update these equations
-            self.q[0] = self.q[0]
-            self.q[1] = self.q[1]
-            self.q[2] = self.q[2]
+            self.q[0] = self.q[0] + dX*np.cos(self.q[2])
+            self.q[1] = self.q[1] + dX*np.sin(self.q[2])
+            self.q[2] = self.q[2] + DT
 
             self.q[2] = wrap_angle(self.q[2])
 
             # Step 2: Calculate the process model Jacobians
             # TODO: Define F and W
-            F = np.array([])
-            W = np.array([])
+            F = np.array([[1, 0, -dX*np.sin(self.q[2])], 
+                        [0, 1, dX*np.cos(self.q[2])], 
+                        [0, 0, 1]])
+
+            W = np.array([[np.cos(self.q[2]), 0], [np.sin(self.q[2]), 0], [0, 1]])
 
             # Step 3: update the covariance estimate
             # TODO: Update this equation
-            self.P = self.P
+            self.P = np.dot(np.dot(F, self.P), np.transpose(F)) + np.dot(np.dot(W, self.Q), np.transpose(W))
 
     def update(self, z: np.ndarray, tag_xy: np.ndarray):
         # z is the measurement in the form [range, bearing]
@@ -45,28 +48,34 @@ class EKF:
 
             # Step 1: calculate the predicted range and bearing measurements
             # TODO: update the equations below
-            rng_pred = 1.0
-            bearing_pred = 0.0
+            dx = tag_xy[0] - self.q[1]
+            dy = tag_xy[1] - self.q[1]
+            r2 = dx**2 + dy**2
+            rng_pred = np.sqrt(r2)
+            bearing_pred = wrap_angle(np.arctan2(dy, dx) - self.q[2])
             z_pred = np.array([rng_pred, bearing_pred])
 
             # Step 2: Calculate the innovation
             # TODO: Define y
-            y = np.array([0.0, 0.0])
+            y = z - z_pred
             y[1] = wrap_angle(y[1])
 
             # Step 3: Calculate the measurement Jacobian
             # TODO: Define H
-            H = np.array([])
+            H = np.array([[-dx/r, -dy/r, 0],
+                        [dy/r2, -dx/r2, -1]])
 
             # Step 4: Calculate the Kalman gain
             # TODO: Define K
-            K = np.array([])
+            S = H @ self.P @ np.transpose(H) + self.R
+            K = self.P @ np.transpose(H) @ np.linalg.inv(S)
 
             # Step 5: Update the state and covariance estimates
             # TODO: Update these equations
-            self.q = self.q
+            self.q = self.q + K @ y
             self.q[2] = wrap_angle(self.q[2])
-            self.P = self.P
+            I = np.eye(3)
+            self.P =(I - K @ H) @ self.P
 
 
 
